583d26595cd81e3648fc2da423ee9aa61768868a
Ismael-schl <ihadi678@insite.4cd.edu>
2026-09-23 20:05:20 -0700

    Final commit, program performs according to Canvas guidelines

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// This code block initiates a class Color with three values for rgb. It uses standard setters/getters and prints a formatted list of its values with member f-n print
 class Color {
     private:
     string name;
@@ -34,18 +35,43 @@ class Color {
     int getbvalue(){
         return bvalue;
     }
-    void print(Color color) {
+    void print() {
         cout << "Color name: " << name << endl;
         cout << "Red value: " << rvalue << endl;
         cout << "Green value: " << gvalue << endl;
-        cout << "Blue value: " << bvalue << endl;
+        cout << "Blue value: " << bvalue << endl << endl;
     }
 };
 
+//The main function creates various colors and populates their values. It also prints them in a formatted way via the use of the print() member function.
 int main() {
+
     Color teal;
     teal.setname("Teal");
     teal.setrvalue(157);
     teal.setgvalue(210);
-    teal.setbvalue(500)
+    teal.setbvalue(500);
+    teal.print();
+
+    Color cyan;
+    cyan.setname("Cyan");
+    cyan.setrvalue(123);
+    cyan.setgvalue(216);
+    cyan.setbvalue(513);
+    cyan.print();
+
+    Color crimson;
+    crimson.setname("Crimson");
+    crimson.setrvalue(700);
+    crimson.setgvalue(59);
+    crimson.setbvalue(176);
+    crimson.print();
+
+    Color burgundy;
+    burgundy.setname("Burgundy");
+    burgundy.setrvalue(400);
+    burgundy.setgvalue(12);
+    burgundy.setbvalue(130);
+    burgundy.print();
+    return 0;
 }
